62ea7ef5c5fed8409b2815b608492c5c701ad4af
suelrin <dancerelrin@gmail.com>
2023-06-20 07:17:20 +0900

    load api

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,4 +1,12 @@
+import { useQuery } from 'react-query';
+import { getMovies } from '../api';
+
+//backdrop_path
+
 function Home() {
+  const { data, isLoading } = useQuery(['movies', 'nowPlaying'], getMovies);
+  console.log(data, isLoading);
+
   return (
     <div style={{ backgroundColor: 'whitesmoke', height: '200vh' }}>home</div>
   );
